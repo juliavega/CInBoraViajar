93222e29a135429c8bf87b7664eca6dcaaa50d05
Tiago de Barros Moraes <tbm2@WINDOWS.CIN.UFPE.BR>
2018-06-11 09:20:45 -0300

    Erros

--- CinBoraViajar/src/classesBasicas/Cliente.java
+++ CinBoraViajar/src/classesBasicas/Cliente.java
@@ -1,10 +1,14 @@
 package classesBasicas;
 
+import erros.cpfInvalidoException;
+
 public class Cliente extends Pessoa {
-	
-	//Atributos exclusivos da classe Cliente
+
+	// Atributos exclusivos da classe Cliente
 	private String passaporte, email, numeroCartao;
-	public Cliente(String nome, String cpf, String dataNascimento, String passaporte, String email, String numeroCartao) {
+
+	public Cliente(String nome, String cpf, String dataNascimento, String passaporte, String email, String numeroCartao)
+			throws cpfInvalidoException {
 		super(nome, cpf, dataNascimento);
 		this.passaporte = passaporte;
 		this.email = email;
--- CinBoraViajar/src/classesBasicas/Funcionario.java
+++ CinBoraViajar/src/classesBasicas/Funcionario.java
@@ -1,10 +1,13 @@
 package classesBasicas;
 
-public class Funcionario extends Pessoa{
+import erros.cpfInvalidoException;
+
+public class Funcionario extends Pessoa {
 	private String cargo;
 	private double salario;
-	
-	public Funcionario(String nome, String cpf, String dataNascimento, String cargo, double salario) {
+
+	public Funcionario(String nome, String cpf, String dataNascimento, String cargo, double salario)
+			throws cpfInvalidoException {
 		super(nome, cpf, dataNascimento);
 		this.cargo = cargo;
 		this.salario = salario;
