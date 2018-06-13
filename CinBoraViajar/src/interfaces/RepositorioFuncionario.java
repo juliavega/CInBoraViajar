1a6f33a9d5669079909a0476da80e92cc15bbeec
Rosa <rosa@usto.re>
2018-06-13 16:42:41 -0300

    Preenchimento do repositório arrayFuncionario

--- CinBoraViajar/src/interfaces/RepositorioFuncionario.java
+++ CinBoraViajar/src/interfaces/RepositorioFuncionario.java
@@ -1,11 +1,12 @@
 package interfaces;
 
 import classesBasicas.Funcionario;
+import erros.LimiteAtingidoException;
 import erros.FuncionarioJaCadastradoException;
 import erros.FuncionarioNaoEncontradoException;
 
 public interface RepositorioFuncionario {
-	public void inserir(Funcionario funcionario) throws FuncionarioNaoEncontradoException, FuncionarioJaCadastradoException;
+	public void inserir(Funcionario funcionario) throws LimiteAtingidoException, FuncionarioJaCadastradoException;
 	public void atualizar(Funcionario funcionario) throws FuncionarioNaoEncontradoException, FuncionarioJaCadastradoException;
 	public void remover(String cpf) throws FuncionarioNaoEncontradoException;
 	public Funcionario procurar(String cpf) throws FuncionarioNaoEncontradoException;
--- CinBoraViajar/src/repositorios/RepositorioArrayFuncionario.java
+++ CinBoraViajar/src/repositorios/RepositorioArrayFuncionario.java
@@ -2,33 +2,66 @@ package repositorios;
 
 import classesBasicas.Funcionario;
 import erros.FuncionarioNaoEncontradoException;
+import erros.LimiteAtingidoException;
 import interfaces.RepositorioFuncionario;
 
 public class RepositorioArrayFuncionario implements RepositorioFuncionario {
+	private Funcionario[] arrayFuncionario = new Funcionario[1000];
+	private int contador = 0;
 
-	@Override
-	public void inserir(Funcionario funcionario) throws FuncionarioNaoEncontradoException {
-		// TODO Auto-generated method stub
-		
+	public void inserir(Funcionario funcionario) throws LimiteAtingidoException {
+		if (this.contador < this.arrayFuncionario.length) {
+			this.arrayFuncionario[contador] = funcionario;
+			this.contador++;
+		} else {
+			LimiteAtingidoException e;
+			e = new LimiteAtingidoException();
+			throw e;
+		}
 	}
 
-	@Override
-	public void atualizar(Funcionario funcionario)
-			throws FuncionarioNaoEncontradoException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void remover(String cpf) throws FuncionarioNaoEncontradoException {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public Funcionario procurar(String cpf) throws FuncionarioNaoEncontradoException {
-		// TODO Auto-generated method stub
-		return null;
+		Funcionario resposta = null;
+		boolean jaAchou = false;
+		for (int i = 0; i <= this.contador && !jaAchou; i++) {
+			if (this.arrayFuncionario[i].getCpf().equals(cpf)) {
+				resposta = this.arrayFuncionario[i];
+				jaAchou = true;
+			}
+		}
+		if (jaAchou) {
+			return resposta;
+		} else {
+			FuncionarioNaoEncontradoException e;
+			e = new FuncionarioNaoEncontradoException();
+			throw e;
+		}
 	}
 
+	public void atualizar(Funcionario funcionario) throws FuncionarioNaoEncontradoException {
+		int indice = this.getIndice(funcionario.getCpf());
+		arrayFuncionario[indice] = funcionario;
+	}
+	
+	public int getIndice(String cpf) throws FuncionarioNaoEncontradoException {
+		int resposta = 0;
+		boolean jaAchou = false;
+		for (int i = 0; i < this.contador && !jaAchou; i++) {
+			if (this.arrayFuncionario[i].getCpf().equals(cpf)) {
+				resposta = i;
+				jaAchou = true;
+			}
+		}
+		if (jaAchou) {
+			return resposta;
+		} else {
+			FuncionarioNaoEncontradoException e;
+			e = new FuncionarioNaoEncontradoException();
+			throw e;
+		}
+	}
 }
