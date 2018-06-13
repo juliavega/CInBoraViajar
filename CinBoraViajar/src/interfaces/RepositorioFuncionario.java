9c4f761d22dd7f937e33ab7ed69ad4b1eaf2e548
ias4 <ias4@g3c21.windows.cin.ufpe.br>
2018-06-13 10:45:31 -0300

    Alterações no atualizar das interfaces

--- CinBoraViajar/src/interfaces/RepositorioFuncionario.java
+++ CinBoraViajar/src/interfaces/RepositorioFuncionario.java
@@ -5,7 +5,7 @@ import erros.FuncionarioNaoEncontradoException;
 
 public interface RepositorioFuncionario {
 	public void inserir(Funcionario funcionario) throws FuncionarioNaoEncontradoException;
-	public void atualizar(Funcionario funcionario) throws FuncionarioNaoEncontradoException;
+	public void atualizar(Funcionario funcionarioAnterior, Funcionario funcionarioNovo) throws FuncionarioNaoEncontradoException;
 	public void remover(Funcionario funcionario) throws FuncionarioNaoEncontradoException;
 	public Funcionario procurar(String cpf) throws FuncionarioNaoEncontradoException;
 }
