4f7e5bf483381e7b7c5c481fa6336e680fddb88d
jlvmv <jlvmv@g3c20.windows.cin.ufpe.br>
2018-06-13 10:13:01 -0300

    Repositorio destino
    para de mandar em mim icaro.

--- CinBoraViajar/src/interfaces/RepositorioDestino.java
+++ CinBoraViajar/src/interfaces/RepositorioDestino.java
@@ -1,5 +1,11 @@
 package interfaces;
 
-public interface RepositorioDestino {
+import classesBasicas.Destino;
+import erros.DestinoNaoEncontradoException;
 
+public interface RepositorioDestino {
+	public void inserir(Destino destino) throws DestinoNaoEncontradoException;
+	public void atualizar(Destino destino) throws DestinoNaoEncontradoException;
+	public void remover(Destino destino) throws DestinoNaoEncontradoException;
+	public Destino procurar(Destino destino) throws DestinoNaoEncontradoException;
 }
