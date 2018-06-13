21cf79cafbc74814aa16998f823bf178dc91d431
ias4 <ias4@g3c21.windows.cin.ufpe.br>
2018-06-13 10:20:26 -0300

    Interface PacoteViagem

--- CinBoraViajar/src/interfaces/RepositorioPacoteViagem.java
+++ CinBoraViajar/src/interfaces/RepositorioPacoteViagem.java
@@ -1,5 +1,11 @@
 package interfaces;
 
-public interface RepositorioPacoteViagem {
+import classesBasicas.PacoteViagem;
+import erros.PacoteNaoEncontradoException;
 
+public interface RepositorioPacoteViagem {
+	public void inserir(PacoteViagem pacoteViagem) throws PacoteNaoEncontradoException;
+	public void atualizar(PacoteViagem pacoteViagem) throws PacoteNaoEncontradoException;
+	public void remover(PacoteViagem pacoteViagem) throws PacoteNaoEncontradoException;
+	public PacoteViagem procurar(PacoteViagem pacoteViagem) throws PacoteNaoEncontradoException;
 }
