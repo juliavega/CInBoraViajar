6da6f45e896ff82b3486df0f7e615c7b21d038ad
jlvmv <jlvmv@g3c20.windows.cin.ufpe.br>
2018-06-13 10:19:45 -0300

    FuncionarioRepositorio

--- CinBoraViajar/src/interfaces/RepositorioFuncionario.java
+++ CinBoraViajar/src/interfaces/RepositorioFuncionario.java
@@ -1,5 +1,11 @@
 package interfaces;
 
-public interface RepositorioFuncionario {
+import classesBasicas.Funcionario;
+import erros.FuncionarioNaoEncontradoException;
 
+public interface RepositorioFuncionario {
+	public void inserir(Funcionario funcionario) throws FuncionarioNaoEncontradoException;
+	public void atualizar(Funcionario funcionario) throws FuncionarioNaoEncontradoException;
+	public void remover(Funcionario funcionario) throws FuncionarioNaoEncontradoException;
+	public Funcionario procurar(Funcionario funcionario) throws FuncionarioNaoEncontradoException;
 }
