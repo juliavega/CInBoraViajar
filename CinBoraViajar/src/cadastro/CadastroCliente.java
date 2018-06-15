e448a83202f20dea1dba710b75f8a0f86db95c1d
Tiago Moraes <tbm2@cin.ufpe.br>
2018-06-15 08:50:34 -0300

    Criado o modelo de classe negócio. Copiem aí.

--- CinBoraViajar/src/cadastro/CadastroCliente.java
+++ CinBoraViajar/src/cadastro/CadastroCliente.java
@@ -1,16 +1,37 @@
 package cadastro;
 
 import classesBasicas.Cliente;
+import erros.ClienteJaCadastradoException;
+import erros.ClienteNaoEncontradoException;
 import erros.LimiteAtingidoException;
 import interfaces.RepositorioCliente;
+import repositorios.RepositorioArrayCliente;
+import repositorios.RepositorioListaCliente;
 
 public class CadastroCliente {
 	private RepositorioCliente clientes;
-	
-	public CadastroCliente(RepositorioCliente rep) {
-		this.clientes = rep;
+
+	public CadastroCliente(boolean tipo) {
+		if (tipo) {
+			this.clientes = new RepositorioListaCliente();
+		} else {			
+			this.clientes = new RepositorioArrayCliente();
+		}
 	}
-	public void cadastrar (Cliente cliente) throws LimiteAtingidoException,  {
+
+	public void cadastrar(Cliente cliente) throws LimiteAtingidoException, ClienteJaCadastradoException {
 		this.clientes.inserir(cliente);
+	}
+
+	public void remover(String cpf) throws ClienteNaoEncontradoException {
+		this.clientes.remover(cpf);
+	}
+
+	public void atualizar(Cliente cliente) throws ClienteNaoEncontradoException, ClienteJaCadastradoException {
+		this.clientes.atualizar(cliente);
+	}
+	
+	public Cliente procurar (String cpf) throws ClienteNaoEncontradoException {
+		return this.clientes.procurar(cpf);
 	}
 }
