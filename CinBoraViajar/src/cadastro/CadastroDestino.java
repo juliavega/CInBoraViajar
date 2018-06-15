823a176f6cf15e51b85b335ac894d2565e929afd
jlvmv <jlvmv@g3c28.windows.cin.ufpe.br>
2018-06-15 09:27:30 -0300

    Pronto

--- CinBoraViajar/src/cadastro/CadastroDestino.java
+++ CinBoraViajar/src/cadastro/CadastroDestino.java
@@ -1,5 +1,36 @@
 package cadastro;
 
-public class CadastroDestino {
+import classesBasicas.Destino;
+import erros.DestinoJaCadastradoException;
+import erros.DestinoNaoEncontradoException;
+import interfaces.RepositorioDestino;
+import repositorios.RepositorioArrayDestino;
+import repositorios.RepositorioListaDestino;
 
+public class CadastroDestino {
+	private RepositorioDestino destinos;
+
+	public CadastroDestino(boolean tipo) {
+		if (tipo) {
+			this.destinos = new RepositorioListaDestino();
+		} else {
+			this.destinos = new RepositorioArrayDestino();
+		}
+	}
+
+	public void cadastrar(Destino destino) throws DestinoNaoEncontradoException, DestinoJaCadastradoException {
+		this.destinos.inserir(destino);
+	}
+
+	public void remover(String cidade) throws DestinoNaoEncontradoException {
+		this.destinos.remover(cidade);
+	}
+
+	public void atualizar(Destino destino) throws DestinoNaoEncontradoException, DestinoJaCadastradoException {
+		this.destinos.atualizar(destino);
+	}
+
+	public void procurar(String cidade) throws DestinoNaoEncontradoException {
+		this.destinos.procurar(cidade);
+	}
 }
