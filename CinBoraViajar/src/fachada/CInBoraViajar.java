76813a32dbe869f7909437e934b951ebde2393bc
tbm2 <tbm2@g3c22.windows.cin.ufpe.br>
2018-06-18 09:56:39 -0300

    Criada a fachada

--- CinBoraViajar/src/fachada/CInBoraViajar.java
+++ CinBoraViajar/src/fachada/CInBoraViajar.java
@@ -6,6 +6,7 @@ import cadastro.CadastroFuncionario;
 import cadastro.CadastroPacoteViagem;
 import classesBasicas.Cliente;
 import classesBasicas.Destino;
+import classesBasicas.Funcionario;
 import classesBasicas.PacoteViagem;
 import erros.ClienteInvalidoException;
 import erros.ClienteJaCadastradoException;
@@ -13,6 +14,8 @@ import erros.ClienteNaoEncontradoException;
 import erros.DestinoInvalidoException;
 import erros.DestinoJaCadastradoException;
 import erros.DestinoNaoEncontradoException;
+import erros.FuncionarioJaCadastradoException;
+import erros.FuncionarioNaoEncontradoException;
 import erros.LimiteAtingidoException;
 import erros.PacoteJaCadastradoException;
 import erros.PacoteNaoEncontradoException;
@@ -23,28 +26,38 @@ public class CInBoraViajar {
 	private CadastroDestino destinos;
 	private CadastroPacoteViagem pacotesViagem;
 	
-	public void cadastrarCliente(Cliente cliente) throws LimiteAtingidoException, ClienteJaCadastradoException{
-		clientes.cadastrar(cliente);
+	public CInBoraViajar(boolean tipo) {
+		clientes = new CadastroCliente(tipo);
+		funcionarios = new CadastroFuncionario(tipo);
+		destinos = new CadastroDestino(tipo);
+		pacotesViagem = new CadastroPacoteViagem(tipo);		
 	}
 	
+	//Cliente
+	public void cadastrarCliente(Cliente cliente) throws LimiteAtingidoException, ClienteJaCadastradoException {
+		clientes.cadastrar(cliente);
+	}
+
 	public void removerCliente(String cpf) throws ClienteNaoEncontradoException {
 		clientes.remover(cpf);
 	}
-	
+
 	public void atualizarCliente(Cliente cliente) throws ClienteNaoEncontradoException {
 		clientes.atualizar(cliente);
 	}
-	
+
 	public Cliente procurarCliente(String cpf) throws ClienteNaoEncontradoException {
 		return clientes.procurar(cpf);
 	}
 	
-	public void cadastrarPacote(PacoteViagem pacote) throws PacoteJaCadastradoException, PacoteNaoEncontradoException, DestinoInvalidoException, ClienteInvalidoException {
+	//Pacote
+	public void cadastrarPacote(PacoteViagem pacote) throws PacoteJaCadastradoException, PacoteNaoEncontradoException,
+			DestinoInvalidoException, ClienteInvalidoException {
 		Destino d = pacote.getDestino();
 		Cliente c = pacote.getCliente();
 		if (destinos.existe(d.getCidade()) && clientes.existe(c.getCpf())) {
 			pacotesViagem.cadastrar(pacote);
-		} else if (!destinos.existe(d.getCidade())){
+		} else if (!destinos.existe(d.getCidade())) {
 			DestinoInvalidoException e;
 			e = new DestinoInvalidoException();
 			throw e;
@@ -54,34 +67,52 @@ public class CInBoraViajar {
 			throw e;
 		}
 	}
-	
+
 	public void removerPacote(String id) throws PacoteNaoEncontradoException {
 		pacotesViagem.remover(id);
 	}
-	
+
 	public void atualizarPacote(PacoteViagem pacote) throws PacoteNaoEncontradoException {
 		pacotesViagem.atualizar(pacote);
 	}
-	
+
 	public PacoteViagem procurarPacote(String id) throws PacoteNaoEncontradoException {
 		return pacotesViagem.procurar(id);
 	}
 	
-	public void cadastrarDestino (Destino destino) throws DestinoNaoEncontradoException, DestinoJaCadastradoException {
+	//Funcionário
+	public void cadastrar(Funcionario funcionario) throws LimiteAtingidoException, FuncionarioJaCadastradoException {
+		funcionarios.cadastrar(funcionario);
+	}
+
+	public void remover(String cpf) throws FuncionarioNaoEncontradoException {
+		funcionarios.remover(cpf);
+	}
+
+	public void atualizar(Funcionario funcionario)
+			throws FuncionarioNaoEncontradoException, FuncionarioJaCadastradoException {
+		funcionarios.atualizar(funcionario);
+	}
+
+	public Funcionario procurar(String cpf) throws FuncionarioNaoEncontradoException {
+		return funcionarios.procurar(cpf);
+	}
+	
+	//Destino
+	public void cadastrarDestino(Destino destino) throws DestinoNaoEncontradoException, DestinoJaCadastradoException {
 		destinos.cadastrar(destino);
 	}
-	
-	public void removerDestino (String cidade) throws DestinoNaoEncontradoException {
+
+	public void removerDestino(String cidade) throws DestinoNaoEncontradoException {
 		destinos.remover(cidade);
 	}
-	
-	public void atualizarDestino (Destino destino) throws DestinoNaoEncontradoException, DestinoJaCadastradoException {
+
+	public void atualizarDestino(Destino destino) throws DestinoNaoEncontradoException, DestinoJaCadastradoException {
 		destinos.atualizar(destino);
 	}
-	
-	public Destino procurarDestino (String cidade) throws DestinoNaoEncontradoException {
+
+	public Destino procurarDestino(String cidade) throws DestinoNaoEncontradoException {
 		return destinos.procurar(cidade);
 	}
-	
-	
+
 }
